c5874f6c2e2efae252162c21cd78e8d041b72a8f
Jack <jackbannistersutton@gmail.com>
2020-02-01 23:11:18 +1300

    Removed raw assets and changed camera

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class CameraScript : MonoBehaviour
 {
     public GameObject player;
+    public int minY = 10;
+    public int LockY = 12;
+    public int HeightDiffBeforeFollow = 30;
     public Vector3 offset;
     
     // Start is called before the first frame update
@@ -15,7 +18,17 @@ public class CameraScript : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-         transform.position = new Vector3 (player.transform.position.x + offset.x, player.transform.position.y + offset.y, offset.z); // Camera follows the player with specified offset position
+    {   
+        float newy = LockY;
+        //float newy = player.transform.position.y + offset.y;
+        //if(player.transform.position.y > transform.position.y+HeightDiffBeforeFollow){
+        //    newy = player.transform.position.y + offset.y - HeightDiffBeforeFollow;
+        //}
+
+        //if(newy < minY){
+        //    newy = minY;
+        //}
+
+        transform.position = new Vector3 (player.transform.position.x + offset.x, newy , offset.z); // Camera follows the player with specified offset position
     }
 }
