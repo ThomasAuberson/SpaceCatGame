4a8c6ccaaba2e4e9f16d85586af92d9c5807ae9a
Thomas Auberson <thomas.auberson@gmail.com>
2020-02-02 16:54:04 +1300

    Raining Asteroids!

---
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+    public GameObject asteroidPrefab;
+    public float spawnDelay = 5f;
+    private float nextAsteroid = 0f;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (nextAsteroid > 0) {
+            nextAsteroid -= Time.deltaTime;
+        }
+        if (nextAsteroid <= 0) {
+            Instantiate(asteroidPrefab, transform.position, Quaternion.identity, null);
+            nextAsteroid = spawnDelay;
+        }
+    }
+}
