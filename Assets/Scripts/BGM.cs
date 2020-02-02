d96bd1277044a5f7d6923ed56e41fa432f4f1a9f
Kilreth <jonathan.pavat.brown@gmail.com>
2020-02-02 16:32:16 +1300

    Make jukebox persist across game scenes so music doesn't restart

---
+++ Assets/Scripts/BGM.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BGM : MonoBehaviour
+{
+    private static BGM instance = null;		// maintain ONE instance of the script/parent object
+    private AudioSource audioSource;
+
+    public AudioClip bgmIntro;
+
+    public static BGM GetInstance()
+    {
+        return instance;
+    }
+
+    // called zero
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(gameObject);
+
+        audioSource = GetComponent<AudioSource>();
+        audioSource.loop = true;
+    }
+
+    // called first
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    // called second
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "MainScene" || scene.name == "RepairMinigameScene")
+        {
+            if (!audioSource.isPlaying)
+            {
+                audioSource.PlayOneShot(bgmIntro);
+                audioSource.PlayScheduled(AudioSettings.dspTime + bgmIntro.length);
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    // called when the game is terminated
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+}
